2ad16ed7da83d11df150359f6445e52c2e393002
Shlomi Lachmish <shlomi.lachmish@sap.com>
2020-01-27 14:49:53 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,90 @@
+# mon-service
+
+easy monitoring cli
+
+[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
+[![Version](https://img.shields.io/npm/v/mon-service.svg)](https://npmjs.org/package/mon-service)
+[![Downloads/week](https://img.shields.io/npm/dw/mon-service.svg)](https://npmjs.org/package/mon-service)
+
+<!-- toc -->
+* [mon-service](#mon-service)
+* [Usage](#usage)
+* [Commands](#commands)
+<!-- tocstop -->
+
+# Usage
+
+<!-- usage -->
+```sh-session
+$ npm install -g mon-service
+$ mon-service COMMAND
+running command...
+$ mon-service (-v|--version|version)
+mon-service/0.0.4 darwin-x64 node-v13.7.0
+$ mon-service --help [COMMAND]
+USAGE
+  $ mon-service COMMAND
+...
+```
+<!-- usagestop -->
+
+# Commands
+
+<!-- commands -->
+* [`mon-service hello`](#mon-service-hello)
+* [`mon-service help [COMMAND]`](#mon-service-help-command)
+* [`mon-service monitor`](#mon-service-monitor)
+
+## `mon-service hello`
+
+Describe the command here
+
+```
+USAGE
+  $ mon-service hello
+
+OPTIONS
+  -n, --name=name  name to print
+
+DESCRIPTION
+  ...
+  Extra documentation goes here
+```
+
+_See code: [src/commands/hello.js](https://github.com/shlomi-lachmish/mon-service/blob/v0.0.4/src/commands/hello.js)_
+
+## `mon-service help [COMMAND]`
+
+display help for mon-service
+
+```
+USAGE
+  $ mon-service help [COMMAND]
+
+ARGUMENTS
+  COMMAND  command to show help for
+
+OPTIONS
+  --all  see all commands in CLI
+```
+
+_See code: [@oclif/plugin-help](https://github.com/oclif/plugin-help/blob/v2.2.3/src/commands/help.ts)_
+
+## `mon-service monitor`
+
+Describe the Monitor command here
+
+```
+USAGE
+  $ mon-service monitor
+
+OPTIONS
+  -f, --file=file  tests object file path
+
+DESCRIPTION
+  ...
+  Extra documentation goes here
+```
+
+_See code: [src/commands/monitor.js](https://github.com/shlomi-lachmish/mon-service/blob/v0.0.4/src/commands/monitor.js)_
+<!-- commandsstop -->
---
+++ src/commands/monitor.js
@@ -0,0 +1,144 @@
+/* eslint-disable no-lonely-if */
+/* eslint-disable no-negated-condition */
+/* eslint-disable object-curly-spacing */
+/* eslint-disable semi */
+/* eslint-disable quotes */
+/* eslint-disable indent */
+const { Command, flags } = require("@oclif/command");
+// const { promisify } = require("util");
+const fs = require("fs");
+const https = require("https");
+// const readFileAsync = promisify(fs.readFile);
+const emojic = require("emojic");
+const chalk = require("chalk");
+const axios = require("axios");
+const instance = axios.create({
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false
+  })
+});
+
+const getData = async element => {
+  try {
+    const thePL = element.hasOwnProperty("payload")
+      ? element.payload
+      : undefined;
+    const response = await instance[element.type.toLowerCase()](
+      element.url,
+      thePL
+    );
+    // console.log(JSON.stringify(Object.keys(response), false, 2));
+    // console.log(`${response.status} : ${response.statusText}`); // statusNumber
+    // console.log(response.headers);
+    // console.log(response.config);
+    // if (thePL) {
+    //   console.log(response.data, thePL);
+    // }
+
+    const data = response.data;
+    let testProp = Object.keys(element.tests)[0];
+    if (testProp !== "status") {
+      // console.log(`testProp = ${testProp}`);
+      // console.log(
+      //   `element.tests[testProp] = ${JSON.stringify(element.tests[testProp])}`
+      // );
+      // console.log(`data[testProp] = ${JSON.stringify(data[testProp])}`);
+      // console.log(
+      //   `(data[testProp] === element.tests[testProp]) = ${data[testProp] ===
+      //     element.tests[testProp]}`
+      // );
+      if (Array.isArray(element.tests[testProp])) {
+        if (data[testProp][0] === element.tests[testProp][0]) {
+          console.log(
+            `${element.name}: ${chalk.bgGreen("Alive")} ${
+              emojic.whiteCheckMark
+            }`
+          );
+          if (thePL) {
+            console.log(
+              `input: ${JSON.stringify(thePL)} , output: ${JSON.stringify(
+                response.data
+              )}`
+            );
+          }
+        } else {
+          console.log(`${element.name}: ${chalk.bgRed("Dead")} ${emojic.x}`);
+        }
+      } else {
+        if (data[testProp] === element.tests[testProp]) {
+          console.log(
+            `${element.name}: ${chalk.bgGreen("Alive")} ${
+              emojic.whiteCheckMark
+            }`
+          );
+        } else {
+          console.log(`${element.name}: ${chalk.bgRed("Dead")} ${emojic.x}`);
+        }
+      }
+    } else {
+      if (response[testProp] === element.tests[testProp]) {
+        console.log(
+          `${element.name}: ${chalk.bgGreen("Alive")} ${emojic.whiteCheckMark}`
+        );
+      } else {
+        console.log(`${element.name}: ${chalk.bgRed("Dead")} ${emojic.x}`);
+      }
+    }
+  } catch (error) {
+    let testProp = Object.keys(element.tests)[0];
+    // console.error(error);
+    if (error.response[testProp] === element.tests[testProp]) {
+      console.log(
+        `${element.name}: ${chalk.bgGreen("Alive")} ${emojic.whiteCheckMark}`
+      );
+    } else {
+      console.log(`${element.name}: ${chalk.bgRed("Dead")} ${emojic.x}`);
+      // console.error(Object.keys(error));
+      // console.error(error.toJSON());
+      // console.error(Object.keys(error.response));
+      console.error(
+        `${chalk.bgRed(error.response.status)} : ${chalk.bgRed(
+          error.response.statusText
+        )}`
+      );
+    }
+    // console.log(`${element.name}: ${chalk.bgRed("Dead")} ${emojic.x}`);
+  }
+};
+
+class MonitorCommand extends Command {
+  async run() {
+    const { flags } = this.parse(MonitorCommand);
+    const fileName = flags.file || "Please add test file path...";
+    this.log(`This is the file name you gave me: ${fileName}`);
+
+    // readFileAsync(axios.get('db.json') .then(//...), { encoding: "utf8" })
+    // const agent = new https.Agent({
+    //   rejectUnauthorized: false
+    // });
+    instance
+      .get(fileName)
+      .then(contents => {
+        // console.log(contents);
+        // let arrTests = JSON.parse(contents);
+        contents.data.forEach(element => {
+          getData(element);
+        });
+      })
+      .catch(error => {
+        console.log(`err: ${error.code}`);
+        throw error;
+      });
+  }
+}
+
+MonitorCommand.description = `Describe the Monitor command here
+...
+Extra documentation goes here
+`;
+
+MonitorCommand.flags = {
+  file: flags.string({ char: "f", description: "tests object file path" })
+};
+
+module.exports = MonitorCommand;
